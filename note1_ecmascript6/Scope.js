a5f85b3e7cce3847923a85d27647e9e9881b4a36
sbpark88 <saebyulpark88@gmail.com>
2021-02-08 10:31:31 +0900

    Scope study done.

--- note1_ecmascript6/Scope.js
+++ note1_ecmascript6/Scope.js
@@ -16,24 +16,92 @@ var -> 함수 레벨 스코프 : 함수 내부에서 선언한 변수는 지역�
 let -> 블록 레벨 스코프 : 코드 블록 내부에서 선언한 변수는 지역변수다.
  */
 // 함수 레벨 스코프
-console.log(foo);   // undefined
-
-var foo = 123;
-console.log(foo);   // 123
-
-{
-    var foo = 335;  // 중복 선언이 허용된다. 그리고 함수 내부가 아니므로 전역변수다. 따라서 전역 변수 var foo = 123;의 값을 335로 바꾼다.
-}
-console.log(foo);   // 335
+// console.log(foo);   // undefined
+//
+// var foo = 123;
+// console.log(foo);   // 123
+//
+// {
+//     var foo = 335;  // 중복 선언이 허용된다. 그리고 함수 내부가 아니므로 전역변수다. 따라서 전역 변수 var foo = 123;의 값을 335로 바꾼다.
+// }
+// console.log(foo);   // 335
 
 // 블록 레벨 스코프
-let too = 123;
-{
-    let too = 324;      // 블록 레벨 스코프로 지역변수다.
-    let bar = 231;
-    console.log(too);   // 324
+// let too = 123;
+// {
+//     let too = 324;      // 블록 레벨 스코프로 지역변수다.
+//     let bar = 231;
+//     console.log(too);   // 324
+// }
+// console.log(too);   // 123
+// console.log(bar);   // ReferenceError: bar is not defined
+
+// let foo = 90;    // 전역변수
+// {
+//     console.log(foo);   // 90
+//     let foo = 23;
+// }
+
+// Closure
+// var arr = [];
+//
+// for (var i = 0; i < 3; i++) {
+//     (function (k) {
+//         arr.push(function () { console.log(k); });
+//     }(i));
+// }
+//
+// for (var j = 0; j < 3; j++) {
+//     arr[j]();
+
+// }
+
+// var arr = [];
+//
+// for (let i = 0; i < 3; i++) {
+//     arr.push(function () { console.log(i); });
+// }
+//
+// // 배열에서 함수를 꺼내어 호출한다.
+// for (var j = 0; j < 3; j++) { arr[j](); }	// 0 1 2
+
+// Const
+// let foo;
+// foo = 15;
+// console.log(foo);
+//
+// const bar;  // SyntaxError: Missing initializer in const declaration
+// bar = 20;
+// console.log(bar);
+
+// const rainbow = { first: `led`
+//                 , second: `orange`
+//                 , thrid: `yello`
+//                 , fourth: `green` };
+//
+// console.log(rainbow);
+//
+// // rainbow = null; // TypeError: Assignment to constant variable.
+//
+// // property는 접근 가능하다.
+// // 수정
+// rainbow.first = `purple`;
+//
+// // 추가
+// rainbow.fifth = `blue`;
+// rainbow['sixth'] = `navy`;
+//
+// // 삭제
+// delete rainbow.thrid;
+//
+// console.log(rainbow);
+
+
+for (let i = 0; i < 3; i ++) {
+    console.log(i);     // 0, 1, 2
 }
-console.log(too);   // 123
-console.log(bar);   // ReferenceError: bar is not defined
 
-
+const MAXROWS = 3;
+for (let i = 0; i < MAXROWS; i ++) {
+    console.log(i);     // 0, 1, 2
+}
